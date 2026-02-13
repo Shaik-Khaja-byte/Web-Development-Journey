aad754cb705c0fb7cd0c6cfc2fcd6bbf50096b5b
Shaik-Khaja-Byte <khaja4756@gmail.com>
2026-02-13 07:35:28 +0530

    Learning to create templates using EJS

---
+++ EJS/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const port = 8080;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"))
+
+app.get("/", (req, res)=>{
+    res.render("home.ejs");
+})
+
+app.get("/home", (req, res)=>{
+    res.send("Hello...");
+})
+
+app.listen(port, ()=>{
+    console.log(`listening to the port ${port}`)
+})
