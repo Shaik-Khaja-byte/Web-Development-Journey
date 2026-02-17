2d5004a33b03c655d6c85812c16a2f3dc830f269
Shaik-Khaja-Byte <khaja4756@gmail.com>
2026-02-17 06:46:45 +0530

    Added modifying instagram.ejs by accessing data from data.json

--- EJS/index.js
+++ EJS/index.js
@@ -23,9 +23,14 @@ app.get("/rollDice", (req, res)=>{
 })
 
 app.get("/ig/:username", (req, res) => {
-    const followers = ["Rahul", "Akanksh", "Alien", "Ajay", "Saraswath", "Shreyas", "Gagan Deep"];
     let {username} = req.params;
-    res.render("instagram.ejs", {username, followers});
+    const instaData = require("./data.json");
+    const data = instaData[username];
+    if(data){
+        res.render("instagram.ejs", {data});
+    } else {
+        res.render("error.ejs")
+    }
 })
 
 app.listen(port, ()=>{
