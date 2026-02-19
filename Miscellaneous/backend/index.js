dad3b2f0c30925b14661d30392a05e2310eb7a6e
Shaik-Khaja-Byte <khaja4756@gmail.com>
2026-02-19 07:15:30 +0530

    Added Handling POST requests

--- Miscellaneous/backend/index.js
+++ Miscellaneous/backend/index.js
@@ -2,13 +2,19 @@ const express = require("express")
 const app = express()
 const port = 8080;
 
+// middlewares used to parse url encoded data and json data
+app.use(express.urlencoded({extended : true}))
+app.use(express.json())
+
 app.get("/register", (req, res)=>{
-    const {username, password} = req.query;
+    let {username, password} = req.query;
     res.send(`standard GET request, welcome ${username}`);
 })
 
-app.post("/request", (req, res)=>{
-    res.send("standard POST request");
+app.post("/register", (req, res)=>{
+    let {username, password} = req.body;
+    console.log(req.body)
+    res.send(`standard POST request, welcome ${username}`);
 })
 
 app.listen(port, ()=>{
