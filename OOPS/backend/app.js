afe8d933c0daf52121f7f4ba6ef14bd74b5b5e72
Shaik-Khaja-Byte <khaja4756@gmail.com>
2026-03-01 18:58:12 +0530

    Added the concept of classes and constructor keyword

--- OOPS/backend/app.js
+++ OOPS/backend/app.js
@@ -45,18 +45,37 @@
 
 // constructors - returns nothing and starts with capital
 
-function Person(name, age){
-    this.name = name;
-    this.age = age;
-}
+// function Person(name, age){
+//     this.name = name;
+//     this.age = age;
+// }
 
-Person.prototype.talk = () => {
-    console.log(`Hello, my name is ${name}`);
+// Person.prototype.talk = () => {
+//     console.log(`Hello, my name is ${name}`);
+// }
+
+// let p1 = new Person("Shaik Khaja", 21)
+// let p2 = new Person("Rahul", 20)
+
+// console.log(p1.talk == p2.talk); // true
+
+// // here the blueprint is given to the constructors and objects are created with the help of new keyword
+
+//-----------------------------------------------------------------------------------------------------------------------------
+
+// classes
+
+class Person {
+    constructor(name, age){ // speacial method for creating and initializing an objectinstance of that class
+        this.name = name;   // the internal implementation of this method is same as the above  
+        this.age = age
+    }
+    talk(){
+        console.log(`Hi, my name is ${name}`);
+    }
 }
 
 let p1 = new Person("Shaik Khaja", 21)
-let p2 = new Person("Rahul", 20)
+let p2 = new Person("Rahul", 21)
 
-console.log(p1.talk == p2.talk); // true
-
-// here the blueprint is given to the constructors and objects are created with the help of new keyword
+console.log(p1.talk === p2.talk) // true
