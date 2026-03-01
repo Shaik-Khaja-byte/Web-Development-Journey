6854f4eaaab34b496bfed8f2f87ded2982c81039
Shaik-Khaja-Byte <khaja4756@gmail.com>
2026-03-01 18:12:40 +0530

    Added the usage of factory functions

--- OOPS/backend/app.js
+++ OOPS/backend/app.js
@@ -1,16 +1,42 @@
-let arr1 = [1,2,3,4]
-let arr2 = [5,6,7]
+// let arr1 = [1,2,3,4]
+// let arr2 = [5,6,7]
 
-arr1.sayHello = () => {
-    console.log("hello");
+// arr1.sayHello = () => {
+//     console.log("hello");
+// }
+
+// arr2.sayHello = () => {
+//     console.log("hello");
+// }
+
+// // let's test
+
+// console.log(arr1.sayHello === arr2.sayHello) // false because both the functions are created separately in the memory
+
+// console.log(arr1.push === arr2.push) // true beacause prototype functions serve as a common template 
+
+
+
+// factory functions
+
+function personMaker(name, age){
+    const person = {
+        name : name,
+        age : age, 
+        talk(){
+            console.log(`Hi, my name is ${name}`)
+        }
+    }
+    return person;
 }
 
-arr2.sayHello = () => {
-    console.log("hello");
-}
+let p1 = personMaker("Shaik Khaja", 21);
+let p2 = personMaker("Rahul", 21);
 
-// let's test
+console.log(p1)
+console.log(p1.talk())
 
-console.log(arr1.sayHello === arr2.sayHello) // false because both the functions are created separately in the memory
+// this is somewhat a better way of creating objects, but still ineffecient
 
-console.log(arr1.push === arr2.push) // true beacause prototype functions serve as a common template 
+// let's see how
+console.log(p1.talk === p2.talk) // false separate copies are created for each object
