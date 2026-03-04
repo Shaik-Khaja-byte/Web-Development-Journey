bab644f9c241a727ba138f9fae541a522faf7f4b
Shaik-Khaja-Byte <khaja4756@gmail.com>
2026-03-04 13:14:36 +0530

    Added the concept of  REST and creating RESTful APIs, created API to view the posts

---
+++ REST/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+
+app.use(express.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+let posts = [
+    {
+        username : "desishaikster",
+        content : "I love coding"
+    },
+    {
+        username : "ananthraj",
+        content : "I love to do research"
+    },
+    {
+        username : "rahulbandi",
+        content : "I got selected for my first internship"
+    },
+]
+
+app.get("/posts", (req, res) => {
+    res.render("index.ejs", {posts});
+})
+
+app.listen(port, ()=>{
+    console.log(`listening to port ${port}`);
+})
