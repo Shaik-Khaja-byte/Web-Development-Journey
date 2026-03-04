a738f42140e42d5431cf598bb65f7877b64a8486
Shaik-Khaja-Byte <khaja4756@gmail.com>
2026-03-04 18:34:49 +0530

    Added the API for deleting a post and changed the CSS of the posts

--- REST/index.js
+++ REST/index.js
@@ -74,6 +74,14 @@ app.get("/posts/:id/edit", (req, res) => {
     res.render("edit.ejs", {post})
 })
 
+// API for deleting the post
+
+app.delete("/posts/:id", (req, res) => {
+    let {id} = req.params;
+    posts = posts.filter((p) => id !== p.id)
+    res.redirect("/posts")
+})
+
 app.listen(port, ()=>{
     console.log(`listening to port ${port}`);
 })
