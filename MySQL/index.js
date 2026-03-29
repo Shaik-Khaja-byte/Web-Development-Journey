3f27f71457133cb58fb757cf2caf55beef8701c9
Shaik-Khaja-Byte <khaja4756@gmail.com>
2026-03-29 15:55:43 +0530

    Added a new route to edit the username, 1) edited via from 2) saved to db

--- MySQL/index.js
+++ MySQL/index.js
@@ -5,8 +5,11 @@ const mysql = require('mysql2');
 const express = require('express');
 const app = express();
 const port = 8080;
-const path = require('path')
+const path = require('path');
+const methodOverride = require('method-override');
 
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({extended : true}));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
@@ -49,6 +52,47 @@ app.get("/users", (req, res) => {
         if(err) throw err;
         res.render("showUsers.ejs", {users});
         })
+    } catch(err) {
+        console.log(err);
+        res.send("some error in DB");
+    }
+})
+
+// Edit route
+app.get("/user/:id/edit", (req, res) => {
+    let {id} = req.params;
+    let q = `SELECT * FROM user WHERE id='${id}'`;
+    try {
+        connection.query(q, (err, result) => {
+        if(err) throw err;
+        let user = result[0];
+        res.render("edit.ejs", {user});
+        })
+    } catch(err) {
+        console.log(err);
+        res.send("some error in DB");
+    }
+})
+
+// Update (DB) route
+app.patch("/user/:id", (req, res) => {
+    let {id} = req.params;
+    let {password: formPassword, username: newUsername} = req.body;
+    let q = `SELECT * FROM user WHERE id='${id}'`;
+    try {
+        connection.query(q, (err, result) => {
+        if(err) throw err;
+        let user = result[0];
+        if(formPassword != user.password){
+            res.send("WRONG Password");
+        } else {
+            let q2 = `UPDATE user SET username = '${newUsername}' WHERE id = '${id}'`;
+            connection.query(q2, (err, result) => {
+                if (err) throw err;
+                res.redirect("/users");
+            });
+        }
+        });
     } catch(err) {
         console.log(err);
         res.send("some error in DB");
