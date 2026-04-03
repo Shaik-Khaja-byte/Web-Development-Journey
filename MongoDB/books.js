0bcfb8ae444268833d96211978bed7ccac604c63
Shaik-Khaja-Byte <khaja4756@gmail.com>
2026-04-03 21:16:23 +0530

    fix: update validations and errors

--- MongoDB/books.js
+++ MongoDB/books.js
@@ -23,7 +23,7 @@ const userSchema = mongoose.Schema({
     },
     price: {
         type: Number,
-        min: 1
+        min: [1, "price is too low to sell"]
     },
     discount: {
         type: Number,
@@ -73,17 +73,28 @@ const Book = mongoose.model("Book", userSchema);
 //     .catch(err => {console.log(err)});
 
 
-const book4 = new Book({
-    title: "Harry Potter 1",
-    author: "IDK",
-    price: -1, // min is 1
-    category: "BL" // no category in enum
-});
+// const book4 = new Book({
+//     title: "Harry Potter 1",
+//     author: "IDK",
+//     price: -1, // min is 1
+//     category: "BL" // no category in enum
+// });
 
-book4
-    .save()
+// book4
+//     .save()
+//     .then(res => {console.log(res)})
+//     .catch(err => {console.log(err)});
+
+
+// Book.findByIdAndUpdate("69cfd1801e3d8058d79c4d54", {price: -500}) // this allows for updation against the schema validation (dangerous)
+//     .then(res => {console.log(res)})
+//     .catch(err => {console.log(err)})
+
+
+// fix
+Book.findByIdAndUpdate("69cfd1801e3d8058d79c4d54", {price: -200}, {runValidators: true})  
     .then(res => {console.log(res)})
-    .catch(err => {console.log(err)});
+    .catch(err => {console.log(err.errors.price.properties.message)})
 
 
 
