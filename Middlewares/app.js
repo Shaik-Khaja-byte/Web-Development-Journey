df1d8c8d8d4f636f999d4e835618ee9e3b07cb90
Shaik-Khaja-Byte <khaja4756@gmail.com>
2026-04-16 13:23:33 +0530

    Understanding the behaviour and properties of middleware

--- Middlewares/app.js
+++ Middlewares/app.js
@@ -1,13 +1,23 @@
 const express = require('express');
-const app = express()
+const app = express();
 const port = 8080;
 
-middleware 
-
-app.get("/", (req, res) => {
-    res.send();
+// for every request, It will send the same response, and it can never reach the below specified routes
+app.use( (req, res) => { 
+    let {query} = req.query;
+    console.log(query);
+    console.log("Hi, I'm a middleware");
+    res.send("Middleware finished");
 })
 
-app.use(port, () => {
+app.get("/", (req, res) => {
+    res.send("Hi, I'm root");
+})
+
+app.get("/random", (req, res) => {
+    res.send("This is a random page");
+})
+
+app.listen(port, () => {
     console.log(`app is listening to port ${port}`);
 })
