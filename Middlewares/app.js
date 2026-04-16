a0952f4dbdd11e21ef26f537cbb3badf5a9bbbd7
Shaik-Khaja-Byte <khaja4756@gmail.com>
2026-04-16 13:48:03 +0530

    feat: Added a utility based middleware

--- Middlewares/app.js
+++ Middlewares/app.js
@@ -2,16 +2,26 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-// for every request, It will send the same response, and it can never reach the below specified routes
-app.use( (req, res, next) => { 
-    console.log("Hi, I'm middleware one");
-    next();
-    console.log("you can writ after next()"); // bad practice
-})
+// // for every request, It will send the same response, and it can never reach the below specified routes
+// app.use( (req, res, next) => { 
+//     console.log("Hi, I'm middleware one");
+//     next();
+//     console.log("you can writ after next()"); // bad practice
+// })
 
-app.use( (req, res, next) => { 
-    console.log("Hi, I'm middleware two");
-    return next(); // next means the end of the middleware
+// app.use( (req, res, next) => { 
+//     console.log("Hi, I'm middleware two");
+//     return next(); // next means the end of the middleware
+// })
+
+
+// creating a utility based middleware
+// we always write the middlewares on the top
+
+app.use( (req, res, next) => {
+    req.time = new Date().toString();
+    console.log(req.method, req.hostname, req.path, req.time);
+    next();
 })
 
 app.get("/", (req, res) => {
