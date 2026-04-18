39d5ded4ee2010108459d0225b38aed4b23834a5
Shaik-Khaja-Byte <khaja4756@gmail.com>
2026-04-18 13:59:15 +0530

    Copied the folser MongoDB with Express by changing the database name to handle the async errors

---
+++ Middlewares/MongoDB with Express/init.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Chat = require("./models/chat.js");
+
+main()
+    .then(() => {console.log("connection successful!")})
+    .catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/fakewhatsapp');
+}
+
+let allChats = [
+    {
+        to: "Shaik Khaja",
+        from: "Shreyas",
+        msg: "Can you send me ML record?",
+        created_at: new Date()
+    },
+    {
+        to: "Rahul",
+        from: "Shaik Khaja",
+        msg: "Can we hangout today?",
+        created_at: new Date()
+    },
+    {
+        to: "Shaik Khaja",
+        from: "Sundeep",
+        msg: "Are you coming to college today?",
+        created_at: new Date()
+    },
+    {
+        to: "Ajay",
+        from: "Shaik Khaja",
+        msg: "Are you coming for lunch today?",
+        created_at: new Date()
+    }
+];
+
+Chat.insertMany(allChats);
+
+
