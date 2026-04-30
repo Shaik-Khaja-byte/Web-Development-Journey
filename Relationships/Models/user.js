4b87993e23698340a50d298bd6693b7c95d9e6fb
Shaik-Khaja-Byte <khaja4756@gmail.com>
2026-04-30 18:12:35 +0530

    Added the one-to-few relationship study

---
+++ Relationships/Models/user.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+main()
+    .then(() => {console.log("Connected to Database.")})
+    .catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/relationDemo');
+}
+
+const userSchema = new Schema({
+    username: String,
+    addresses: [
+        {
+            _id: false,
+            location: String,
+            city: String
+        }
+    ]
+});
+
+const User = mongoose.model("User", userSchema);
+
+const addUser = async () => {
+    let user1 = new User({
+        username: "desishaikster",
+        addresses: [
+            {
+                location: "5th Avenue Street",
+                city: "New York"
+            
+            }
+        ]
+    })
+    user1.addresses.push({location: "H1B Wall Street", city: "New York"});
+    let result = await user1.save();
+    console.log(result);
+}
+
+
+addUser();
+
+
+
